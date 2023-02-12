3c68c8c22263a2cdde4644a6ddaad8112804d854
Dmytro <kotykhin_d@ukr.net>
2023-02-13 00:04:13 +0100

    footer update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Typography } from "@mui/material";
+import { Typography, Container } from "@mui/material";
 import { Box } from "@mui/system";
 
 import styles from './footer.module.scss';
@@ -8,9 +8,11 @@ import styles from './footer.module.scss';
 const Footer: React.FC = () => {
     return (
         <Box className={styles.footer}>
-            <Typography className={styles.footer__logo}>
-                TodoList
-            </Typography>
+            <Container maxWidth='xl'>
+                <Typography className={styles.footer__logo}>
+                    TodoList
+                </Typography>
+            </Container>
         </Box>
     )
 }
