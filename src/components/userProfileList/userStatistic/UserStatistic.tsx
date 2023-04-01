c8f21f43655b202600210c3f9dc0cbefa6878cdf
Dmytro <kotykhin_d@ukr.net>
2023-04-01 11:38:14 +0200

    task completed update

--- src/components/userProfileList/userStatistic/UserStatistic.tsx
+++ src/components/userProfileList/userStatistic/UserStatistic.tsx
@@ -13,48 +13,62 @@ const UserStatistic: React.FC = () => {
 
     return (
         <Paper elevation={10} className={styles.statistic}>
-            <Typography className={styles.title}>
-                {isLoading ? "Loading..." : "Your statistic:"}
-            </Typography>
-            {data?.totalTasks ?
-                <Box className={styles.subtitle}>
-                    <Box className={styles.box}>
-                        <Typography>
-                            {"Total tasks:"}
+            {isLoading ?
+                <Typography className={styles.title}>
+                    {"Loading..."}
+                </Typography> :
+                <>
+                    <Typography className={styles.title}>
+                        {"Your statistic:"}
+                    </Typography>
+                    {data?.totalTasks ?
+                        <Box className={styles.subtitle}>
+                            <Box className={styles.box}>
+                                <Typography>
+                                    {"Total tasks:"}
+                                </Typography>
+                                <Typography>
+                                    {data.totalTasks}
+                                </Typography>
+                            </Box>
+                            <Box className={styles.box}>
+                                <Typography>
+                                    {"Active tasks:"}
+                                </Typography>
+                                <Typography>
+                                    {data.activeTasks}
+                                </Typography>
+                            </Box>
+                            <Box
+                                className={styles.box}
+                                sx={data.overdueTasks ? { color: '#ff0000' } : null}
+                            >
+                                <Typography>
+                                    {"Overdue tasks:"}
+                                </Typography>
+                                <Typography>
+                                    {data.overdueTasks}
+                                </Typography>
+                            </Box>
+                            <Box
+                                className={styles.box}
+                                sx={data.completedTasks ? { color: '#00a1b6' } : null}
+                            >
+                                <Typography>
+                                    {"Completed tasks:"}
+                                </Typography>
+                                <Typography>
+                                    {data.completedTasks}
+                                </Typography>
+                            </Box>
+                        </Box>
+                        :
+                        <Typography className={styles.text}>
+                            {"You don't have any task..."}
                         </Typography>
-                        <Typography>
-                            {data?.totalTasks}
-                        </Typography>
-                    </Box>
-                    <Box className={styles.box}>
-                        <Typography>
-                            {"Active tasks:"}
-                        </Typography>
-                        <Typography>
-                            {data?.activeTasks}
-                        </Typography>
-                    </Box>
-                    <Box className={styles.box}>
-                        <Typography>
-                            {"Completed tasks:"}
-                        </Typography>
-                        <Typography>
-                            {data?.completedTasks}
-                        </Typography>
-                    </Box>
-                    <Box className={styles.box} sx={data?.overdueTasks ? { color: '#ff0000' } : null}>
-                        <Typography>
-                            {"Overdue tasks:"}
-                        </Typography>
-                        <Typography>
-                            {data?.overdueTasks}
-                        </Typography>
-                    </Box>
-                </Box>
-                :
-                <Typography className={styles.text}>
-                    {"You don't have any task..."}
-                </Typography>}
+                    }
+                </>
+            }
         </Paper>
     )
 }
